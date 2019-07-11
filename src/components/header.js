1535725a87bf63cc41d444f00bb50b0dde43e1e0
JoeMulick <joemulick@gmail.com>
2019-07-10 22:51:20 -0700

    getting ready to install material ui

--- src/components/header.js
+++ src/components/header.js
@@ -1,36 +1,16 @@
-import React from 'react'
-import { Link, graphql, useStaticQuery } from 'gatsby'
+import React from "react"
+// import { Link } from "gatsby"
 
-import headerStyles from './header.module.scss'
+// import headerStyles from "./header.module.scss"
+
 
 const Header = () => {
 
-    const data = useStaticQuery(graphql`
-        query{
-            site {
-                siteMetadata {
-                    title
-                }
-            }
-        }
-    `)
-
-    return (
-        <header>
-            <h1>
-                <Link className={headerStyles.header} to='/'>
-                    {data.site.siteMetadata.title}
-                </Link>
-            </h1>
-            <nav>
-                <ul className={headerStyles.navList}>
-                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/'>home</Link></li>
-                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/about'>about</Link></li>
-                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/contact'>contact</Link></li>
-                </ul>
-            </nav>
-        </header>
-    )
+  return (
+    <header>
+      Navbar
+    </header>
+  )
 }
 
 export default Header
