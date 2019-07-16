5edb467372f38e378f86e0cede46281f131cd5cf
JoeMulick <joemulick@gmail.com>
2019-07-15 22:41:56 -0700

    started editing main section text

--- src/components/MainLandingSectionAlpha.js
+++ src/components/MainLandingSectionAlpha.js
@@ -1,34 +1,22 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import ScrollAnimation from 'react-animate-on-scroll';
 
 import mainLandingSectionAlphaStyles from "../styles/mainLandingSectionAlpha.module.scss"
 
-const useStyles = makeStyles(theme => ({
-  myAnimation: {
-  animationDuration: '5s',
-  animationDelay: '5s',
-  }
-}));
+
 
 
 export default function App() {
 
-  const classes = useStyles();
-
   return (
     <div className={mainLandingSectionAlphaStyles.greyImageOverlay}>
         <Container maxWidth="xl">  
-          <ScrollAnimation duration="6" animateIn="fadeIn">
-            <h1 className={mainLandingSectionAlphaStyles.myAnimation}>Some Text</h1>
-            <h1 className={mainLandingSectionAlphaStyles.myAnimation}>Some Text</h1>
-            <h1 className={mainLandingSectionAlphaStyles.myAnimation}>Some Text</h1>
-            <h1 className={mainLandingSectionAlphaStyles.myAnimation}>Some Text</h1>
-            <h1 className={mainLandingSectionAlphaStyles.myAnimation}>Some Text</h1>
-            <h1 className={mainLandingSectionAlphaStyles.myAnimation}>Some Text</h1>
-            <h1 className={mainLandingSectionAlphaStyles.myAnimation}>Some Text</h1>
-          </ScrollAnimation>
+          <div className={mainLandingSectionAlphaStyles.mainTextDiv}>
+            <ScrollAnimation duration="4" animateIn="fadeIn">
+              Empowering students through personalized and expertly guided college and financial aid strategies.
+            </ScrollAnimation>
+          </div>
         </Container>
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function App() {
 
     <MainLandingSectionAlpha />
 
-    {/* <Navbar /> */}
+    <Navbar />
 
     <Container maxWidth="xl">
       <Box>
