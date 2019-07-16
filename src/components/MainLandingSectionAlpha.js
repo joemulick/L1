6d82501a29f4058e48eca66b5dff44d8c85aafc7
JoeMulick <joemulick@gmail.com>
2019-07-15 20:19:29 -0700

    converted MainLandingSectionAlpha into a react class component from a stateless functional component

--- src/components/MainLandingSectionAlpha.js
+++ src/components/MainLandingSectionAlpha.js
@@ -1,32 +1,29 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 // import Typography from '@material-ui/core/Typography';
 // import MuiLink from '@material-ui/core/Link';
 // import Link from '../components/Link';
-
-
+import { withStyles } from '@material-ui/core/styles';
 
 import mainLandingSectionAlphaStyles from "../styles/mainLandingSectionAlpha.module.scss"
 
-const useStyles = makeStyles(theme => ({
-  testStyle: {
-    padding: '150px'
-  }
-}));
+const styles = {
 
+};
 
-export default function App() {
-
-  const classes = useStyles();
-
-  return (
-    <div className={mainLandingSectionAlphaStyles.greyImageOverlay}>
+class MainLandingSectionAlpha extends React.Component {
+  render() {
+    return (
+      <div className={mainLandingSectionAlphaStyles.greyImageOverlay}>
         <Container maxWidth="xl">  
-          <div className={classes.testStyle}>
+          <div className={mainLandingSectionAlphaStyles.test}>
             TEST
           </div>
         </Container>
     </div>
-  );
+    );
+  }
 }
+
+
+export default withStyles(styles)(MainLandingSectionAlpha);
