2351c3c61476bce44e86205371f196ba04f54753
Joseph Mulick <joseph.mulick@Build.Internal>
2019-07-22 16:02:06 -0700

    service modals working

--- src/components/IndexSectionTwoModal.js
+++ src/components/IndexSectionTwoModal.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import SvgIcon from '@material-ui/core/SvgIcon';
+import Button from '@material-ui/core/Button';
+
+import ServiceModalOne from './serviceModals/ServiceModalOne';
+import ServiceModalTwo from './serviceModals/ServiceModalTwo';
 
 import indexSectionTwoStyles from "../styles/indexSectionTwoModal.module.scss"
 
@@ -17,14 +22,12 @@ function LightBulbIcon(props) {
   );
 }
 
-const useStyles = makeStyles(theme => ({
+const styles = {
   root: {
     flexGrow: 1,
   },
   paper: {
-    padding: theme.spacing(2),
     textAlign: 'center',
-    color: theme.palette.text.secondary,
     minHeight: '325px',
     background: '#fff',
     marginTop: '5vh',
@@ -34,7 +37,6 @@ const useStyles = makeStyles(theme => ({
   },
   lightBulb: {
     verticalAlign: 'middle',
-    marginRight: theme.spacing(1),
     marginTop: '2%',
     width: 80,
     height: 80,
@@ -50,14 +52,51 @@ const useStyles = makeStyles(theme => ({
     textAlign: 'center',
     marginTop: '7vh'  
   }
+};
 
-}));
+class IndexSectionTwoModal extends React.Component {
 
-export default function FullWidthGrid() {
+  state = {
+    openOne: false,
+    openTwo: false,
+    // openThree: false,
 
-  const classes = useStyles();
+  };
 
-  return (
+  handleClickOpenOne = () => {
+    this.setState({
+      openOne: true,
+    });
+  };
+  handleClickOpenTwo = () => {
+    this.setState({
+      openTwo: true,
+    });
+  };
+  // handleClickOpenThree = () => {
+  //   this.setState({
+  //     openThree: true,
+  //   });
+  // };
+  
+
+  handleCloseOne = valueOne => {
+    this.setState({ openOne: false });
+  };
+  handleCloseTwo = valueTwo => {
+    this.setState({ openTwo: false });
+  };
+  // handleCloseThree = valueThree => {
+  //   this.setState({ openThree: false });
+  // };
+  
+
+  render() {
+
+    const { classes, onClose, selectedValue, ...other } = this.props;
+
+    return (
+
     <div className={indexSectionTwoStyles.sectionContainer}>
     <Container maxWidth="lg">
         <div className={classes.root}>
@@ -73,6 +112,13 @@ export default function FullWidthGrid() {
                             <LightBulbIcon className={classes.lightBulb} />
                             <h2 className={classes.cardTitle}>Advise Package</h2>
                             <p className={classes.cardText}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry</p>
+                            <Button variant="outlined" onClick={this.handleClickOpenOne}>
+                                learn more
+                                </Button>
+                                <ServiceModalOne
+                                open={this.state.openOne}
+                                onClose={this.handleCloseOne}
+                              />
                         </div>
                     </ScrollAnimation>
                     </Grid>
@@ -85,6 +131,13 @@ export default function FullWidthGrid() {
                             <LightBulbIcon className={classes.lightBulb} />
                             <h2>Transfer Package</h2>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry</p>
+                            <Button variant="outlined" onClick={this.handleClickOpenTwo}>
+                                learn more
+                                </Button>
+                                <ServiceModalTwo
+                                open={this.state.openTwo}
+                                onClose={this.handleCloseTwo}
+                              />
                         </div>
                         </ScrollAnimation>
                     </Grid> 
@@ -92,5 +145,12 @@ export default function FullWidthGrid() {
         </div>
     </Container>
     </div>
-  );
+    );
+  }
 }
+
+IndexSectionTwoModal.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(IndexSectionTwoModal);
