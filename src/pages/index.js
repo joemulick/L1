0c29a822201bba568944e50f3f4eff837ffc2c6a
JoeMulick <joemulick@gmail.com>
2019-08-06 21:48:03 -0700

    ..

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Navbar from '../components/Navbar'
 import MainLandingSectionAlpha from '../components/indexComponents/MainLandingSectionAlpha'
-// import IndexSectionTwoHover from '../components/IndexSectionTwoHover'
 import IndexSectionTwoModal from '../components/indexComponents/IndexSectionTwoModal'
 import IndexSectionThreeExpPanel from '../components/indexComponents/IndexSectionThreeExpPanel'
 
