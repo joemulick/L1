093c6f934ce857c65d7162385f3e6f66a7f0656a
Joseph Mulick <joseph.mulick@Build.Internal>
2019-10-22 15:30:55 -0700

    removed all navbar / footer from layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
-import Footer from '../components/footer'
-import Navbar from '../components/navbar'
+
 
 import layoutStyles from '../styles/layout.module.scss'
 
@@ -10,11 +9,11 @@ import layoutStyles from '../styles/layout.module.scss'
 const Layout = (props) => {
     return(
         <div>
-            <Navbar />
+
                 <div className={layoutStyles.navPadding}>
                     {props.children}
                 </div>
-            <Footer />
+
         </div>
     )
 }
