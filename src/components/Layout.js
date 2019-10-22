08db26ff2eac711e02e1a0cfa5272ec0442f49e9
Joseph Mulick <joseph.mulick@Build.Internal>
2019-10-22 15:34:47 -0700

    reverted layout back to include footer  and navbar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-
+import Footer from '../components/footer.js'
+import Navbar from '../components/navbar.js'
 
 import layoutStyles from '../styles/layout.module.scss'
 
@@ -9,11 +10,11 @@ import layoutStyles from '../styles/layout.module.scss'
 const Layout = (props) => {
     return(
         <div>
-
+            <Navbar />
                 <div className={layoutStyles.navPadding}>
                     {props.children}
                 </div>
-
+            <Footer />
         </div>
     )
 }
